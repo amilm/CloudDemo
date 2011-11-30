a566fd4c9230f0cad609277bb85086b54f74e718
amil <amilmartin@gmail.com>
2011-11-29 23:58:08 -0500

    adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudDemo::Application.config.secret_token = 'ad616a73c37428481edd849aaa9360203a873378f84ba4b322ec7fc0c756089ce2e99027eab310cf9ded32a13b20f271799a110797bd57da475a6126efa78f81'
